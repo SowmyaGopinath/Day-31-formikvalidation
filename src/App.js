329dcd0e7ba3b00147abe51109d819d0287c003e
SowmyaGopinath <sowmi.2694@gmail.com>
2023-10-02 15:44:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import {  Route, Routes } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Listassignbooks from './Listassignbooks';
+import Createbook from './Createbook';
+import Editassignbooks from './Editassignbooks';
+import Listbooks from './Listbooks';
+import Createstudent from './Createstudent';
+import Editbook from './Editbook';
+import Liststudents from './Liststudents';
+import Assignbooks from './Assignbooks';
+import Editstudent from './Editstudent';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+    <div>
+      
+    <Routes>
+        <Route path="/" element={ <Dashboard/> } />
+        <Route path="Createstudent" element={ <Createstudent/> } />
+        <Route path="Editstudent/:userId" element={ <Editstudent/> } />
+        <Route path="Liststudents" element={ <Liststudents/> } />
+        <Route path="Createbook" element={ <Createbook/> } />
+        <Route path="Editbook/:userId" element={ <Editbook/> } />
+        <Route path="Listbooks" element={ <Listbooks/> } />
+        <Route path="Assignbooks" element={ <Assignbooks/> } />
+        <Route path="Editassignbooks/:userId" element={ <Editassignbooks/> } />
+        <Route path="Listassignbooks" element={ <Listassignbooks/> } />
+      </Routes>
     </div>
   );
-}
+} 
 
 export default App;
